75fe36ddedbdcc0442697c93356c6bdd3ed185db
maxusmax2 <maxusmax2@gmail.com>
2023-01-30 19:03:19 +0300

    Создание тестовых классов для изучения работы Spring

---
+++ Back-End/src/main/java/BoardMeet/Backend/Model/TestEntityRepository.java
@@ -0,0 +1,7 @@
+package BoardMeet.Backend.Model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TestEntityRepository extends CrudRepository<TestEntity,Integer> {
+
+}
---
+++ Back-End/src/test/java/BoardMeet/Backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package BoardMeet.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
