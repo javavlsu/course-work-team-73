dd08d7bea4a741eeb84c9d1355d17375ba95fc25
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-03-21 19:24:26 +0300

    Добавлен Exception вызывающийся при неправильном типе файла

---
+++ Back-End/src/main/java/BoardMeet/Backend/Exception/NotAccessExtensionException.java
@@ -0,0 +1,7 @@
+package BoardMeet.Backend.Exception;
+
+public class NotAccessExtensionException extends  Exception {
+    public NotAccessExtensionException (){
+        super("NOT ACCESS EXTENSION!!!");
+    }
+}
