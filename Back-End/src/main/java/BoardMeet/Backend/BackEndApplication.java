407527c40f42643e7710bf8f80e46e2649ef9c35
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-03-22 15:49:12 +0300

    Добавлена работа с Ролями

--- Back-End/src/main/java/BoardMeet/Backend/BackEndApplication.java
+++ Back-End/src/main/java/BoardMeet/Backend/BackEndApplication.java
@@ -3,9 +3,14 @@ package BoardMeet.Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableGlobalMethodSecurity(
+		prePostEnabled = true,
+		securedEnabled = true,
+		jsr250Enabled = true)
 public class BackEndApplication {
 
 	public static void main(String[] args) {
