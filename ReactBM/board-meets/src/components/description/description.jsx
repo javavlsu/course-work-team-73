3d937f40e97c4f1a629f1c4fb4d11595d3076203
Vivi <100605523+FawnVivin@users.noreply.github.com>
2023-04-15 15:46:10 +0300

    Изменен компонент описания игры
    Изменен адрес получения картинки игры

--- ReactBM/board-meets/src/components/description/description.jsx
+++ ReactBM/board-meets/src/components/description/description.jsx
@@ -1,16 +1,20 @@
 import { Rules } from "../icons/icons";
 import style from "./description.module.css";
 
-export const Description = ({game, url}) => {
+export const Description = ({ game, url }) => {
   return (
     <div className={style.wrapper}>
       <p className={style.title}>Описание</p>
       <p className={style.content}>{game?.description}</p>
-      <a className={style.rulesFile} href={url + game?.rule} target="_blank" rel="noopener noreferrer">
-        <Rules/>
+      <a
+        className={style.rulesFile}
+        href={`${url}static/rule/${game?.rule}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Rules />
         <p className={style.rulesIcon}>Правила игры</p>
       </a>
     </div>
-
-  )
-}
+  );
+};
