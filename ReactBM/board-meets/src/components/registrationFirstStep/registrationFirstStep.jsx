0b4db365f55a9c6c86109a26f3cb9780fd3410b7
Vivi <100605523+FawnVivin@users.noreply.github.com>
2023-04-15 16:08:05 +0300

    Изменены компоненты первого и второго этапа регистрации
    Рефакторинг кода

--- ReactBM/board-meets/src/components/registrationFirstStep/registrationFirstStep.jsx
+++ ReactBM/board-meets/src/components/registrationFirstStep/registrationFirstStep.jsx
@@ -2,9 +2,12 @@ import { NavLink } from "react-router-dom";
 import style from "./registrationFirstStep.module.css";
 import { Back } from "../icons/icons";
 
-
-export const RegistrationFirstStep = ({ register, firstStepHandler, handleSubmit, errors }) => {
-
+export const RegistrationFirstStep = ({
+  register,
+  firstStepHandler,
+  handleSubmit,
+  errors,
+}) => {
   return (
     <form className={style.form} onSubmit={handleSubmit(firstStepHandler)}>
       <p className={style.formTitle}>Регистрация</p>
@@ -17,102 +20,125 @@ export const RegistrationFirstStep = ({ register, firstStepHandler, handleSubmit
             required: "введите свой никнейм",
             pattern: {
               value: /^([a-zA-z0-9_]{1,23})$/,
-              message: "допустимые символы (A-Z,a-z,0-9,_)"
+              message: "допустимые символы (A-Z,a-z,0-9,_)",
             },
             maxLength: {
               value: 15,
-              message: "макс.длина-15"
+              message: "макс.длина-15",
             },
             minLength: {
               value: 3,
-              message: "мин.длина-3"
-            }
-          })} />
+              message: "мин.длина-3",
+            },
+          })}
+        />
         {errors?.nickname && <p className="error">{errors.nickname.message}</p>}
         <label className={style.swimLabel}>Nickname</label>
       </div>
       <div className={style.formInput}>
         <label className={style.mandatoryLabel}>Обязательно</label>
-        <input className={style.input} placeholder=" " {...register("name", {
-          required: "введите свое имя",
-          pattern: {
-            value: /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/,
-            message: "некоррекный ввод"
-          },
-          maxLength: {
-            value: 23,
-            message: "макс.длина-23"
-          },
-          minLength: {
-            value: 2,
-            message: "мин.длина-2"
-          }
-        })} />
+        <input
+          className={style.input}
+          placeholder=" "
+          {...register("name", {
+            required: "введите свое имя",
+            pattern: {
+              value: /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/,
+              message: "некоррекный ввод",
+            },
+            maxLength: {
+              value: 23,
+              message: "макс.длина-23",
+            },
+            minLength: {
+              value: 2,
+              message: "мин.длина-2",
+            },
+          })}
+        />
         {errors?.name && <p className="error">{errors.name.message}</p>}
         <label className={style.swimLabel}>Имя</label>
       </div>
       <div className={style.formInput}>
         <label className={style.mandatoryLabel}>Обязательно</label>
-        <input className={style.input} placeholder=" " {...register("surname", {
-          required: "введите свою фамилия",
-          pattern: {
-            value: /^([А-Я]{1}[а-яё]{1,30}|[A-Z]{1}[a-z]{1,30})$/,
-            message: "некорректный ввод"
-          },
-          maxLength: {
-            value: 30,
-            message: "макс.длина-30"
-          },
-          minLength: {
-            value: 2,
-            message: "мин.длина-2"
-          }
-        })} />
+        <input
+          className={style.input}
+          placeholder=" "
+          {...register("surname", {
+            required: "введите свою фамилия",
+            pattern: {
+              value: /^([А-Я]{1}[а-яё]{1,30}|[A-Z]{1}[a-z]{1,30})$/,
+              message: "некорректный ввод",
+            },
+            maxLength: {
+              value: 30,
+              message: "макс.длина-30",
+            },
+            minLength: {
+              value: 2,
+              message: "мин.длина-2",
+            },
+          })}
+        />
         {errors?.surname && <p className="error">{errors.surname.message}</p>}
         <label className={style.swimLabel}>Фамилия</label>
       </div>
       <div className={style.formInput}>
         <label className={style.mandatoryLabel}>Обязательно</label>
-        <input className={style.input} placeholder=" " {...register("aboutMe", {
-          required: "введите информацию о себе",
-          maxLength: {
-            value: 40,
-            message: "макс.длина-40"
-          },
-          minLength: {
-            value: 3,
-            message: "мин.длина-3"
-          }
-        })} />
+        <input
+          className={style.input}
+          placeholder=" "
+          {...register("aboutMe", {
+            required: "введите информацию о себе",
+            maxLength: {
+              value: 40,
+              message: "макс.длина-40",
+            },
+            minLength: {
+              value: 3,
+              message: "мин.длина-3",
+            },
+          })}
+        />
         <label className={style.swimLabel}>Обо мне..</label>
         {errors?.aboutMe && <p className="error">{errors.aboutMe.message}</p>}
       </div>
       <div className={style.formInput}>
         <label className={style.mandatoryLabel}>Обязательно</label>
-        <input className={style.input} placeholder=" " {...register("city", {
-          required: "введите свой город",
-          pattern: {
-            value: /^([a-zA-Zа-яА-ЯёЁ]+[-]?[a-zA-Zа-яА-ЯёЁ]*[-]?[a-zA-Zа-яА-ЯёЁ]*[-]?[a-zA-Zа-яА-ЯёЁ]*)$/,
-            message: "некорректный ввод"
-          },
-          maxLength: {
-            value: 15,
-            message: "макс.длина-15"
-          },
-          minLength: {
-            value: 3,
-            message: "мин.длина-3"
-          }
-        })} />
+        <input
+          className={style.input}
+          placeholder=" "
+          {...register("city", {
+            required: "введите свой город",
+            pattern: {
+              value:
+                /^([a-zA-Zа-яА-ЯёЁ]+[-]?[a-zA-Zа-яА-ЯёЁ]*[-]?[a-zA-Zа-яА-ЯёЁ]*[-]?[a-zA-Zа-яА-ЯёЁ]*)$/,
+              message: "некорректный ввод",
+            },
+            maxLength: {
+              value: 15,
+              message: "макс.длина-15",
+            },
+            minLength: {
+              value: 3,
+              message: "мин.длина-3",
+            },
+          })}
+        />
         {errors?.city && <p className="error">{errors.city.message}</p>}
         <label className={style.swimLabel}>Город</label>
       </div>
-      <input type="submit" className={style.formButton} value="Продолжить" ></input>
+      <input
+        type="submit"
+        className={style.formButton}
+        value="Продолжить"
+      ></input>
       <div className={style.links}>
-        <NavLink to="/" className={style.navLink}><Back />Продолжить без регистрации</NavLink>
+        <NavLink to="/" className={style.navLink}>
+          <Back />
+          Продолжить без регистрации
+        </NavLink>
       </div>
     </form>
-
-
   );
-}
+};
--- ReactBM/board-meets/src/components/registrationSecondStep copy/registrationSecondStep.jsx
+++ ReactBM/board-meets/src/components/registrationSecondStep copy/registrationSecondStep.jsx
@@ -2,13 +2,21 @@ import { NavLink } from "react-router-dom";
 import style from "./registrationSecondStep.module.css";
 import { Back } from "../icons/icons";
 
-
-export const RegistrationSecondStep = ({ register, handleSubmit, onSubmit, secondStepHandler, errors, watch }) => {
+export const RegistrationSecondStep = ({
+  register,
+  handleSubmit,
+  onSubmit,
+  secondStepHandler,
+  errors,
+  watch,
+}) => {
   return (
     <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
       <p className={style.formTitle}>Регистрация</p>
       <div className={style.formInput}>
-        <label htmlFor="email" className={style.mandatoryLabel}>Обязательно</label>
+        <label htmlFor="email" className={style.mandatoryLabel}>
+          Обязательно
+        </label>
         <input
           type="email"
           id="email"
@@ -18,23 +26,32 @@ export const RegistrationSecondStep = ({ register, handleSubmit, onSubmit, secon
             required: "введите свой email",
             pattern: {
               value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-              message: "email введен не корректно"
-            }
-          })} />
+              message: "email введен не корректно",
+            },
+          })}
+        />
         {errors?.email && <p className="error">{errors.email.message}</p>}
-        <label className={style.swimLabel} htmlFor="email">Email</label>
+        <label className={style.swimLabel} htmlFor="email">
+          Email
+        </label>
       </div>
       <div className={style.formInput}>
-        <label htmlFor="role" className={style.mandatoryLabel}>Mandatory</label>
+        <label htmlFor="role" className={style.mandatoryLabel}>
+          Mandatory
+        </label>
         <select className={style.input} placeholder=" " {...register("role")}>
           <option>player</option>
           <option>organization</option>
           <option>publisher</option>
         </select>
-        <label className={style.swimLabel} htmlFor="role" >Роль</label>
+        <label className={style.swimLabel} htmlFor="role">
+          Роль
+        </label>
       </div>
       <div className={style.formInput}>
-        <label htmlFor="password" className={style.mandatoryLabel}>Обязательно</label>
+        <label htmlFor="password" className={style.mandatoryLabel}>
+          Обязательно
+        </label>
         <input
           type="password"
           className={style.input}
@@ -42,20 +59,24 @@ export const RegistrationSecondStep = ({ register, handleSubmit, onSubmit, secon
           {...register("password", {
             required: "введите пароль",
             pattern: {
-              value: /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/,
-              message: "должен включать (A-Z,a-z,0-9,(*/@#%$^&))"
+              value:
+                /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/,
+              message: "должен включать (A-Z,a-z,0-9,(*/@#%$^&))",
             },
             maxLength: {
               value: 15,
-              message: "макс.длина-15"
+              message: "макс.длина-15",
             },
             minLength: {
               value: 6,
-              message: "мин.длина-6"
-            }
-          })} />
+              message: "мин.длина-6",
+            },
+          })}
+        />
         {errors?.password && <p className="error">{errors.password.message}</p>}
-        <label className={style.swimLabel} htmlFor="password" >Пароль</label>
+        <label className={style.swimLabel} htmlFor="password">
+          Пароль
+        </label>
       </div>
       <div className={style.formInput}>
         <label className={style.mandatoryLabel}>Обязательно</label>
@@ -68,19 +89,34 @@ export const RegistrationSecondStep = ({ register, handleSubmit, onSubmit, secon
               if (watch("password") != val) {
                 return "пароли не совпадают";
               }
-            }
-          })} />
-        {errors?.doublePassword && <p className="error">{errors.doublePassword.message}</p>}
+            },
+          })}
+        />
+        {errors?.doublePassword && (
+          <p className="error">{errors.doublePassword.message}</p>
+        )}
         <label className={style.swimLabel}>Повторите пароль</label>
       </div>
 
-      <input type="submit" className={style.formButton} value="Зарегистрироваться"></input>
+      <input
+        type="submit"
+        className={style.formButton}
+        value="Зарегистрироваться"
+      ></input>
       <div className={style.links}>
-        <button type="button" className={style.backButton} onClick={secondStepHandler}><Back />Обратно</button>
-        <NavLink to="/" className={style.navLink}><Back />Продолжить без регистрации</NavLink>
+        <button
+          type="button"
+          className={style.backButton}
+          onClick={secondStepHandler}
+        >
+          <Back />
+          Обратно
+        </button>
+        <NavLink to="/" className={style.navLink}>
+          <Back />
+          Продолжить без регистрации
+        </NavLink>
       </div>
     </form>
-
-
   );
-}
+};
