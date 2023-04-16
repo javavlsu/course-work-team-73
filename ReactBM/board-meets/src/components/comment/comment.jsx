aaad04736d6103e6d0d3e4359b986058c9fa114a
Vivi <100605523+FawnVivin@users.noreply.github.com>
2023-04-16 12:36:13 +0300

    Рефакторинг компонента Comment

--- ReactBM/board-meets/src/components/comment/comment.jsx
+++ ReactBM/board-meets/src/components/comment/comment.jsx
@@ -2,24 +2,41 @@ import axios from "axios";
 import { NavLink } from "react-router-dom";
 import { getConfig } from "../../helpers/getConfig";
 import { getUser } from "../../helpers/getUser";
-import { Age, Close, Players, Rating, TimeBold, Сomplexity } from "../icons/icons";
+import {
+  Age,
+  Close,
+  Players,
+  Rating,
+  TimeBold,
+  Сomplexity,
+} from "../icons/icons";
 import style from "./comment.module.css";
 
 export const Comment = ({ comment, delComment, url }) => {
   const user = getUser();
 
-  const delHandler = () =>{
-    axios.delete(`${url}comments/${comment.id}`, getConfig())
-    .then(() => delComment(comment))
-  }
-  
+  const delHandler = () => {
+    axios
+      .delete(`${url}comments/${comment.id}`, getConfig())
+      .then(() => delComment(comment));
+  };
+
   return (
     <div className={style.container}>
       <div className={style.commentBody}>
-        <img src={`${url}static/avatar/${comment.author.avatarUrl}`} alt="author" />
-        <NavLink to={`/user/${user?.id}`} className={style.author}>@{comment.author?.username}</NavLink>
+        <img
+          src={`${url}static/avatar/${comment.author.avatarUrl}`}
+          alt="author"
+        />
+        <NavLink to={`/user/${user?.id}`} className={style.author}>
+          @{comment.author?.username}
+        </NavLink>
         <ul className={style.rating}>
-          {user&&(user?.roles[0].name=="admin")?<button className={style.delButton} onClick={delHandler}><Close width="8" height="8" /></button>:null}
+          {user && user?.roles[0].name == "admin" ? (
+            <button className={style.delButton} onClick={delHandler}>
+              <Close width="8" height="8" />
+            </button>
+          ) : null}
           <li className={style.ratingItem}>
             <span>{comment.rating}</span>
             <Rating />
@@ -29,9 +46,12 @@ export const Comment = ({ comment, delComment, url }) => {
             <Сomplexity />
           </li>
           <li className={style.ratingItem}>
-            <span>{comment.bestPlayerMin}-{comment.bestPlayerMax}</span>
+            <span>
+              {comment.bestPlayerMin}-{comment.bestPlayerMax}
+            </span>
             <div className={style.bestPalyers}>
-              Best<Players fill="#057402" />
+              Best
+              <Players fill="#057402" />
             </div>
           </li>
           <li className={style.ratingItem}>
@@ -47,4 +67,4 @@ export const Comment = ({ comment, delComment, url }) => {
       </div>
     </div>
   );
-}
+};
