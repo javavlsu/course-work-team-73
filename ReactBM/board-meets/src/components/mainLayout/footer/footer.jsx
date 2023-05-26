48851d7279ead1d3cc680291f04b5164305f159d
Vivi <100605523+FawnVivin@users.noreply.github.com>
2023-05-26 18:57:31 +0300

    Рефакторинг

--- ReactBM/board-meets/src/components/mainLayout/footer/footer.jsx
+++ ReactBM/board-meets/src/components/mainLayout/footer/footer.jsx
@@ -3,12 +3,25 @@ import style from "./footer.module.css";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 export const Footer = () => {
-    const {t, i18n} = useTranslation();
-    const [lang, setLang] = useState('en')
+  const { t, i18n } = useTranslation();
   return (
     <footer className={style.footer}>
-      <button type="button" className={i18n.language === 'ru'? style.flagBtnActive: style.flagBtn} onClick={()=>{i18n.changeLanguage('ru')}}><RuFlag/></button>
-      <button type="button" className={i18n.language === 'en'? style.flagBtnActive: style.flagBtn}  onClick={()=>i18n.changeLanguage('en')}><EnFlag/></button>
+      <button
+        type="button"
+        className={i18n.language === "ru" ? style.flagBtnActive : style.flagBtn}
+        onClick={() => {
+          i18n.changeLanguage("ru");
+        }}
+      >
+        <RuFlag />
+      </button>
+      <button
+        type="button"
+        className={i18n.language === "en" ? style.flagBtnActive : style.flagBtn}
+        onClick={() => i18n.changeLanguage("en")}
+      >
+        <EnFlag />
+      </button>
     </footer>
   );
 };
