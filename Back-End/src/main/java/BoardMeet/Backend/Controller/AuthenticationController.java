b726aa83565f3a78d13d5166c7864d821b063813
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2023-05-28 18:09:36 +0300

    Логика логина была перенесена в сервис авторизации

--- Back-End/src/main/java/BoardMeet/Backend/Controller/AuthenticationController.java
+++ Back-End/src/main/java/BoardMeet/Backend/Controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package BoardMeet.Backend.Controller;
 import BoardMeet.Backend.DTO.AuthenticationRequestDTO;
 import BoardMeet.Backend.Model.User;
 import BoardMeet.Backend.Security.jwt.JwtTokenProvider;
+import BoardMeet.Backend.Service.AuthenticationService;
 import BoardMeet.Backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,31 +24,22 @@ import java.util.Map;
 @RequestMapping(value = "/api/auth/")
 public class AuthenticationController {
     private  final AuthenticationManager authenticationManager;
+    private  final AuthenticationService authenticationService;
     private  final JwtTokenProvider jwtTokenProvider;
-    private  final UserService userService;
+
     @Autowired
-    public AuthenticationController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserService userService) {
+    public AuthenticationController(AuthenticationManager authenticationManager, AuthenticationService authenticationService, JwtTokenProvider jwtTokenProvider) {
         this.authenticationManager = authenticationManager;
+        this.authenticationService = authenticationService;
         this.jwtTokenProvider = jwtTokenProvider;
-        this.userService = userService;
     }
     @PostMapping("login")
     public ResponseEntity login(@RequestBody AuthenticationRequestDTO requestDto){
-        try{
-            String username = requestDto.getUsername();
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,requestDto.getPassword()));
-            User user = userService.findByUsername(username);
 
-            if(user == null){
-                throw  new UsernameNotFoundException(username);
-            }
-            String token = jwtTokenProvider.createToken(username);
-            Map<Object,Object> response = new HashMap<>();
-            response.put("authUser",user);
-            response.put("token",token);
-            return  ResponseEntity.ok(response);
-        }catch (AuthenticationException e){
-            throw  new BadCredentialsException("invalid username or password");
+        try{
+            return  ResponseEntity.ok(authenticationService.login(requestDto));
+        }catch (BadCredentialsException e){
+            throw  e;
         }
 
     }
